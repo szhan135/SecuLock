8d7ab8f282cac942e7653a4f22cb97a7ccb60623
mnico005 <mnico005@ucr.edu>
2020-05-24 19:42:37 -0700

    Switched to SPI communication

--- RaspPiAtmega/Rasp2Atmega/main.c
+++ RaspPiAtmega/Rasp2Atmega/main.c
@@ -20,23 +20,48 @@
 #include "timer.h"
 #include "Nokia_5110.h" 
 
+
+#define SS       DDB4
+#define MOSI     DDB5
+#define MISO     DDB6
+#define SCK      DDB7
+
+void SPI_ServantInit(void) {
+	/* Set MISO output, all others input */
+	DDRB &= ~(1<<MISO) | (1<<SCK) | (1<<SS);
+	DDRB |= (1<<MISO);
+	/* Enable SPI */
+	SPCR = ((1<<SPE)); //| (1<<SPIE));
+}
+
+char SPI_SlaveReceive(short data) {
+	SPDR = data;
+	while(!(SPSR & (1<<SPIF)))
+	;
+	return SPDR;
+}
+
+
+unsigned short Lock_Info = 3;
+char curr_state = 0;
 enum LCD_States {Init, Locked, RFID, FPscan, Unlocked, Locking} LCDstate;
 void LCD_Tick(){
 	switch(LCDstate){
 		case Init:
 			if (!GetBit(PIND, 0))
 			{
-				N5110_clear();
+				//N5110_clear();
 				LCDstate = Unlocked;
 			}else{
-				N5110_clear();
+				//N5110_clear();
 				LCDstate = Locked;
 			}
 			break;
 		case Locked:
-			if (GetBit(PINA, 0))
+			if (Lock_Info == 1)
 			{
-				N5110_clear();
+				Lock_Info = 3;
+				//N5110_clear();
 				LCDstate = RFID;
 			}else{
 				LCDstate = Locked;
@@ -45,7 +70,7 @@ void LCD_Tick(){
 		case RFID:
 			if (!GetBit(PINA, 2))
 			{
-				N5110_clear();
+				//N5110_clear();
 				LCDstate = FPscan;
 			}else{
 				LCDstate = RFID;
@@ -54,16 +79,17 @@ void LCD_Tick(){
 		case FPscan:
 			if (!GetBit(PINA, 3))
 			{
-				N5110_clear();
+				//N5110_clear();
 				LCDstate = Unlocked;
 			}else{
 				LCDstate = FPscan;
 			}
 			break;
 		case Unlocked:
-			if (GetBit(PINA, 1))
+			if (Lock_Info == 2)
 			{
-				N5110_clear();
+				Lock_Info = 3;
+				//N5110_clear();
 				LCDstate = Locked;
 			}else{
 				LCDstate = Unlocked;
@@ -75,24 +101,29 @@ void LCD_Tick(){
 	}
 	switch(LCDstate){
 		case Init:
+			curr_state = 0;
 			break;
 		case Locked:
 			PORTD = 0;
-			lcd_setXY(0x80, 0x40 +2);
-			N5110_Data("LOCKED");
+			curr_state = 1;
+			//lcd_setXY(0x80, 0x40 +2);
+			//N5110_Data("LOCKED");
 			break;
 		case RFID:
-			lcd_setXY(0x80, 0x40 +2);
-			N5110_Data("SCAN RFID");
+			curr_state = 2;
+			//lcd_setXY(0x80, 0x40 +2);
+			//N5110_Data("SCAN RFID");
 			break;
 		case FPscan:
-			lcd_setXY(0x80, 0x40 +2);
-			N5110_Data("SCAN FINGER");
+			curr_state = 3;
+			//lcd_setXY(0x80, 0x40 +2);
+			//N5110_Data("SCAN FINGER");
 			break;
 		case Unlocked:
+			curr_state = 4;
 			PORTD = 1;
-			lcd_setXY(0x80, 0x40 +2);
-			N5110_Data("UNLOCKED");
+			//lcd_setXY(0x80, 0x40 +2);
+			//N5110_Data("UNLOCKED");
 			break;
 		default:
 			break;
@@ -103,15 +134,17 @@ int main(void)
 {
     DDRA = 0x00; PORTA = 0xFF;
 	DDRD = 0xFF; PORTD = 0x00;
-	SPI_Init();
-	N5110_init();
-	N5110_clear();
-	lcd_setXY(0x9F ,0x42);
-	lcd_setXY(0x80, 0x40 +2);
+	SPI_ServantInit();
+	//SPI_Init();
+	//N5110_init();
+	//N5110_clear();
+	//lcd_setXY(0x9F ,0x42);
+	//lcd_setXY(0x80, 0x40 +2);
 	PORTD = 1;
     while (1) 
     {
 		LCD_Tick();
+		Lock_Info = SPI_SlaveReceive(curr_state);
 		//PORTD = 0;
     }
 }
