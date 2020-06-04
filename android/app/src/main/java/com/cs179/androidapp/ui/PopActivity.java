26d289a0cb5e75c2ed4761a0132b320985271b94
szhan135 <33235045+szhan135@users.noreply.github.com>
2020-06-04 12:58:48 -0700

    Update again

--- android/app/src/main/java/com/cs179/androidapp/ui/PopActivity.java
+++ android/app/src/main/java/com/cs179/androidapp/ui/PopActivity.java
@@ -17,17 +17,19 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 package com.cs179.androidapp.ui;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.cs179.androidapp.Injection;
 import com.cs179.androidapp.LocksApp;
@@ -37,17 +39,27 @@ import com.cs179.androidapp.services.AnalyticsService;
 import com.cs179.androidapp.viewmodels.LockListViewModel;
 
 import java.util.HashMap;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.WindowManager;
 
 public class PopActivity extends Activity {
+    private boolean twoPane = false;
+
+    /**
+     * The view model
+     */
+    //private LockListViewModel viewModel = ViewModelProviders.of(com.cs179.androidapp.ui.LockListActivity).get(LockListViewModel.class);
+
+    private AnalyticsService analyticsService = Injection.getAnalyticsService();
+
+    String token;
+    private FragmentManager supportFragmentManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        token =  getIntent().getStringExtra("token");
         setContentView(R.layout.activity_pop);
-
+        Button button2;
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
@@ -60,5 +72,55 @@ public class PopActivity extends Activity {
         params.x = 0;
         params.y = -20;
         getWindow().setAttributes(params);
+        button2 = (Button) findViewById(R.id.button2);
+        //button2.setOnClickListener((View v) -> loadLockDetailFragment("new"));
+        //final LockListAdapter adapter = new LockListAdapter((Lock item) -> loadLockDetailFragment(item.getLockId()));
+
+        // Create the swipe-to-delete handler
+        //LockListViewModel viewModel;
+        //SwipeToDelete swipeHandler = new SwipeToDelete(this, (Lock item) -> viewModel.removeLock(item.getLockId()));
+        //ItemTouchHelper swipeToDelete = new ItemTouchHelper(swipeHandler);
+
+        // Configure the lock list
+        //RecyclerView lock_list = findViewById(R.id.lock_list);
+        //swipeToDelete.attachToRecyclerView(lock_list);
+        //lock_list.setAdapter(adapter);
+
+        // Ensure the lock list is updated whenever the repository is updated
+        //viewModel.getLocksList().observe((LifecycleOwner) this, adapter::submitList);
+    }
+    /*private void loadLockDetailFragment(String lockId) {
+        if (twoPane) {
+            Fragment fragment = new LockDetailFragment();
+            Bundle arguments = new Bundle();
+            arguments.putString(LocksApp.ITEM_ID, lockId);
+            fragment.setArguments(arguments);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.lock_detail_container, fragment)
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, LockDetailActivity.class);
+            intent.putExtra(LocksApp.ITEM_ID, lockId);
+            intent.putExtra("token", token);
+            startActivity(intent);
+        }
+    }*/
+    public void onResume() {
+        super.onResume();
+        HashMap<String,String> attributes = new HashMap<>();
+        attributes.put("twoPane", twoPane ? "true" : "false");
+        analyticsService.recordEvent("LockListActivity", attributes, null);
+    }
+
+
+
+    public FragmentManager getSupportFragmentManager() {
+        return supportFragmentManager;
+    }
+
+    public void setSupportFragmentManager(FragmentManager supportFragmentManager) {
+        this.supportFragmentManager = supportFragmentManager;
+        
     }
 }
